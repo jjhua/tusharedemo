62de74d6340912ba724726e11d2091b2f4002528
home_win10 <gengzhh@gmail.com>
2017-02-10 11:09:40 +0800

    operate not juct today, when is 0, check last until != 0

--- macd_checker.py
+++ macd_checker.py
@@ -191,7 +191,7 @@ def checkAll():
 
 def check_stock_now(code, name):
     operate = 0
-        
+    last_operate = 0
     
     for i in range(0, 1):
         df = ts.get_hist_data(code, start=begin_time_check_now)
@@ -199,7 +199,7 @@ def check_stock_now(code, name):
             break
         print 'retry ', i, code, name
     if df is None:
-        return 0
+        return (0,0)
 
     df = df.sort_index(0)
     dflen = df.shape[0]
@@ -224,19 +224,19 @@ def check_stock_now(code, name):
         curdf['macdhist']=pd.Series(macdhist,index=curdf.index)#DIFF-DEA
         MAlen = len(SignalMA5)
         
-        #2个数组 1.DIFF、DEA均为正，DIFF向上突破DEA，买入信号。 2.DIFF、DEA均为负，DIFF向下跌破DEA，卖出信号。
-        #待修改
-        if curdf.iat[(dflen-1),macd_index]>0:
-            if curdf.iat[(dflen-1),macdsignal_index]>0:
-                if curdf.iat[(dflen-1),macd_index]>curdf.iat[(dflen-1),macdsignal_index] and curdf.iat[(dflen-2),macd_index]<=curdf.iat[(dflen-2),macdsignal_index]:
-                    operate = operate + 10#买入
-                    df['macd_DIFF_DEA'][dflen - 1] = 1
-        else:
-            if curdf.iat[(dflen-1),macdsignal_index]<0:
-                if curdf.iat[(dflen-1),macd_index] == curdf.iat[(dflen-2),macdsignal_index]:
-                    operate = operate - 10#卖出
-                    df['macd_DIFF_DEA'][dflen - 1] = -1
-        
+#        #2个数组 1.DIFF、DEA均为正，DIFF向上突破DEA，买入信号。 2.DIFF、DEA均为负，DIFF向下跌破DEA，卖出信号。
+#        #待修改
+#        if curdf.iat[(dflen-1),macd_index]>0:
+#            if curdf.iat[(dflen-1),macdsignal_index]>0:
+#                if curdf.iat[(dflen-1),macd_index]>curdf.iat[(dflen-1),macdsignal_index] and curdf.iat[(dflen-2),macd_index]<=curdf.iat[(dflen-2),macdsignal_index]:
+#                    operate = operate + 10#买入
+#                    df['macd_DIFF_DEA'][dflen - 1] = 1
+#        else:
+#            if curdf.iat[(dflen-1),macdsignal_index]<0:
+#                if curdf.iat[(dflen-1),macd_index] == curdf.iat[(dflen-2),macdsignal_index]:
+#                    operate = operate - 10#卖出
+#                    df['macd_DIFF_DEA'][dflen - 1] = -1
+
         #3.DEA线与K线发生背离，行情反转信号。
         if curdf.iat[(dflen-1),7]>=curdf.iat[(dflen-1),8] and curdf.iat[(dflen-1),8]>=curdf.iat[(dflen-1),9]:#K线上涨
             if SignalMA5[MAlen-1]<=SignalMA10[MAlen-1] and SignalMA10[MAlen-1]<=SignalMA20[MAlen-1]: #DEA下降
@@ -246,26 +246,38 @@ def check_stock_now(code, name):
             if SignalMA5[MAlen-1]>=SignalMA10[MAlen-1] and SignalMA10[MAlen-1]>=SignalMA20[MAlen-1]: #DEA上涨
                 operate = operate + 1
                 df['macd_DEA_K'][dflen - 1] = -1
-                   
-           
-        #4.分析MACD柱状线，由负变正，买入信号。
-        if curdf.iat[(dflen-1),macdhist_index]>0 and dflen >30 :
-            for i in range(1,26):
-                if curdf.iat[(dflen-1-i),macdhist_index]<=0:#
-#                    operate = operate + 5
-                    df['macd_MACD_SELF'][dflen - 1] = 1
-                    break
-                #由正变负，卖出信号   
-        if curdf.iat[(dflen-1),macdhist_index]<0 and dflen >30 :
-            for i in range(1,26):
-                if curdf.iat[(dflen-1-i),macdhist_index]>=0:#
-#                    operate = operate - 5
-                    df['macd_MACD_SELF'][dflen - 1] = -1
-                    break
+        last_operate = operate
+        if operate == 0:
+            for dflen in range(dflen - 1, 36):
+                if curdf.iat[(dflen-1),7]>=curdf.iat[(dflen-1),8] and curdf.iat[(dflen-1),8]>=curdf.iat[(dflen-1),9]:#K线上涨
+                    if SignalMA5[MAlen-1]<=SignalMA10[MAlen-1] and SignalMA10[MAlen-1]<=SignalMA20[MAlen-1]: #DEA下降
+                        last_operate = -1
+                        df['macd_DEA_K'][dflen - 1] = 1
+                elif curdf.iat[(dflen-1),7]<=curdf.iat[(dflen-1),8] and curdf.iat[(dflen-1),8]<=curdf.iat[(dflen-1),9]:#K线下降
+                    if SignalMA5[MAlen-1]>=SignalMA10[MAlen-1] and SignalMA10[MAlen-1]>=SignalMA20[MAlen-1]: #DEA上涨
+                        last_operate = 1
+                        df['macd_DEA_K'][dflen - 1] = -1
+                if last_operate != 0:
+                   break
+
+#        #4.分析MACD柱状线，由负变正，买入信号。
+#        if curdf.iat[(dflen-1),macdhist_index]>0 and dflen >30 :
+#            for i in range(1,26):
+#                if curdf.iat[(dflen-1-i),macdhist_index]<=0:#
+##                    operate = operate + 5
+#                    df['macd_MACD_SELF'][dflen - 1] = 1
+#                    break
+#                #由正变负，卖出信号   
+#        if curdf.iat[(dflen-1),macdhist_index]<0 and dflen >30 :
+#            for i in range(1,26):
+#                if curdf.iat[(dflen-1-i),macdhist_index]>=0:#
+##                    operate = operate - 5
+#                    df['macd_MACD_SELF'][dflen - 1] = -1
+#                    break
                 
         df['macd_SUM'][dflen - 1] = operate
 
-    return operate
+    return (operate,last_operate)
 
 def checkStockNowInThread((index,row)):
 #    print index
@@ -274,13 +286,13 @@ def checkStockNowInThread((index,row)):
     name = row['name']
     code_str = str(code).zfill(6)
 #    print code_str, '=', name
-    operate = check_stock_now(code_str, name)
+    operate, last_operate = check_stock_now(code_str, name)
     success_count = 0
     failed_count = 0
     if operate != 0:
         success_count,failed_count = check_stock(code_str, name)
 
-    return (index,code_str,name,operate,success_count,failed_count)
+    return (index,code_str,name,operate,last_operate,success_count,failed_count)
 
 def checknow():
     all_stock = pd.read_csv('all.csv')
@@ -290,14 +302,14 @@ def checknow():
     pool.close()
     pool.join()
 
-    for index,code_str,name,operate,success_count,failed_count in results:
+    for index,code_str,name,operate,last_operate,success_count,failed_count in results:
 #        print index, success_count, failed_count
         all_stock.loc[index, 'operate'] = operate
-        if operate != 0:
+        if (operate != 0) or (last_operate != 0):
             all_stock.loc[index, 'macd_success'] = success_count
             all_stock.loc[index, 'macd_fail'] = failed_count
             if (success_count > 3) and (failed_count == 0):
-                print code_str, name, '  operate=', operate,
+                print code_str, name, '  operate=', operate,'  last_operate=', last_operate,
                 print '  success_count=', success_count,'  failed_count=', failed_count
 
 
@@ -329,8 +341,8 @@ def checkSome():
         check_stock(code_str, 'test')
 
 if __name__ == '__main__':    
-#    checknow()
-    checkAll()
+    checknow()
+#    checkAll()
 #    checkSome()
 #    check_stock('000001', 'test')
     print 'finish'
